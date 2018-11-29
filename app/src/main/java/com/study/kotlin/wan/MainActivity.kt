ad1e61f8e1005eae11387e75533a93a77aa7339e
Wynne <a664966415@gmail.com>
2018-11-29 22:20:23 +0800

    Koltin 语法学习

--- app/src/main/java/com/study/kotlin/wan/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/wan/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.study.kotlin.wan
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tv_test).setOnClickListener {
+            Log.d("XXW", "Test")
+
+        }
     }
+
+
 }
