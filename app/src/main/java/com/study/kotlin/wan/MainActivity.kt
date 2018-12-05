91c332c5b8912b1ed276180d954cb8b8fc58b582
wynne <a664966415@gmail.com>
2018-12-05 18:23:39 +0800

    kotlin study

--- app/src/main/java/com/study/kotlin/wan/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/wan/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        main();
+    }
+
+
+    private fun main(args: Array<String>) {
+        println("Hello World")
     }
 }
